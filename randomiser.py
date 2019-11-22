36fa24cc972866241a23e9484ad0c246d89d3371
valtsu41 <42544217+valtsu41@users.noreply.github.com>
2019-11-22 07:48:52 +0200

    Updated the randomiser

--- randomiser.py
+++ randomiser.py
@@ -1,14 +1,23 @@
 #Used for generating the randomised bundle.
 
 import markovify
-import os
+import os, sys
 
+try:
+	bundlepath = os.listdir("./Mindustry/core/assets/bundles")
+except FileNotFoundError:
+	print("Could not locate Mindustry bundles at 'Mindustry/core/assets/bundles'")
+	sys.exit()
 
-for filename in os.listdir("./original"):
+for filename in bundlepath:
 	print("Generating: " + filename)
 
-	input = open("./original/" + filename, "r")
-	output = open("./bundles/" + filename, "w")
+	input = open("./Mindustry/core/assets/bundles/" + filename, "r")
+	try:
+		output = open("./bundles/" + filename, "w")
+	except FileNotFoundError:
+		print("Could not find 'bundles' directory for output")
+		sys.exit()
 	
 	inputstring = input.read()
 	marko = markovify.NewlineText(inputstring, well_formed=False, state_size=2)
@@ -21,8 +30,13 @@ for filename in os.listdir("./original"):
 			output.write("\n")
 		else:
 			startstate = (splitline[0], "=")
-			chainoutput = marko.make_sentence(tries=100, init_state=startstate, test_output=False)
+			try:
+				chainoutput = marko.make_sentence(tries=100, init_state=startstate, test_output=False)
+			except KeyError:
+				chainoutput = str(startstate) + "oh no"
 			output.write(chainoutput + "\n")
 		
 	input.close()
 	output.close()
+
+print("Finished generating bundles")
