930b08e3cc2200772c205b6425b99d1537a7a984
valtsu41 <42544217+valtsu41@users.noreply.github.com>
2019-11-28 14:41:21 +0200

    Improved algorithm

--- randomiser.py
+++ randomiser.py
@@ -11,7 +11,6 @@ except FileNotFoundError:
 
 
 for filename in bundlepath:
-	print("Generating: " + filename)
 
 	input = open("./Mindustry/core/assets/bundles/" + filename, "r")
 	try:
@@ -19,25 +18,39 @@ for filename in bundlepath:
 	except FileNotFoundError:
 		print("Could not find 'bundles' directory for output")
 		sys.exit()
-	
-	inputstring = input.read()
-	marko = markovify.NewlineText(inputstring, well_formed=False, state_size=2)
-	
 	input.seek(0)
 	
+	inputstring = ""
 	for line in input:
-		splitline = line.split(" ")
-		if splitline[0] == "\n":
-			output.write("\n")
-		else:
+		splitline = line.split(" = ")
+		if splitline[0] != "\n":
 			try:
-				chainoutput = marko.make_sentence(tries=10000, max_words=(len(splitline) - 2), test_output=False)
-				if chainoutput == None:
-					raise KeyError
-			except KeyError:
-				chainoutput = "".join(splitline[0]) + " = " + "oh no"
-			output.write(chainoutput + "\n")
+				inputstring += splitline[1]
+			except IndexError:
+				print("Invalid bundle file format")
+				sys.exit()
 		
+	marko = markovify.NewlineText(inputstring, well_formed=False, state_size=2)
+	input.seek(0)
+	
+	lineamount = sum(1 for line in input)
+	input.seek(0)
+	
+	for linenum, line in enumerate(input):
+		print("Generating: " + filename + " (" + str(linenum) + "/" + str(lineamount) + ")", end="\r")
+		splitline = line.split(" ")
+		try:
+			chainoutput = marko.make_sentence(tries=10000, max_words=(len(splitline) - 2), test_output=False)
+			if chainoutput == None:
+				raise KeyError
+		except KeyError:
+			chainoutput = "".join(splitline[0]) + " = " + "oh no"
+		output.write(splitline[0] + "=" + chainoutput + "\n")
+		if len(splitline) - 2 == 0:
+			print("EORKRBWNENEKDLELELEKEKEkdkekennebebeK")
+			sys.exit()
+	
+	print("Generating: " + filename + " (COMPLETED)", end="\n")
 	input.close()
 	output.close()
 
